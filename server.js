d1d34aa7be91f381b2f70040c76979d709f2a88d
Evgeny Karasev <gnomskg@gmail.com>
2017-10-29 22:06:24 +0300

    fix event strean for synchronization data between  server and clients

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const MessageType = {
   
   CLIENTS: '@PEER/CLIENTS',  
   PEER_DATA: '@PEER/DATA',
+
   REQUEST_PEER: '@PEER/REQUEST',
   REQUEST_PEER_ANSWER: '@PEER/REQUEST_ANSWER',
 
@@ -22,37 +23,36 @@ const io = require('socket.io')(server);
 io.set('origins', 'localhost:*');
 
 let clients = Object.create(null);
+let clientData = Object.create(null);
 
 io.on('connection', (socket) => {
   console.log('connection created');
   
   const clientId = crypto.randomBytes(32).toString('hex');
-  let clientData = null;
 
   socket.emit(MessageType.CLIENTS, Object.keys(clients));
-
-  Object.keys(clients)
-  .forEach((key) => clients[key].emit(MessageType.SET_PEER, clientId));
   
   clients[clientId] = socket;
 
   socket.on(MessageType.PEER_DATA, (peer) => {
-    clientData = peer;
-  })
-
-  socket.on(MessageType.REQUEST_PEER, (clientId) => {
-    clients[clientId].emit(MessageType.REQUEST_PEER_ANSWER, clientData);
-  })
-  
-  socket.on(MessageType.DISCONNECT, function () {
-    console.log('disconnect ' + clientId);
-    clients[clientId].disconnect(true);
-    delete clients[clientId];
-    Object.keys(clients).forEach((key) => {
-      clients[key].emit(MessageType.LEAVE_PEER, clientId);
+    console.log('add peer data');
+    clientsData[clientData] = peer;
+    Object.keys(clients)
+    .filter(peerId => !Object.is(peerId, clientId))
+    .forEach(peerId => { 
+      clients[peerId].emit(MessageType.SET_PEER, clientId);
     });
-  });
+  })
 
+  socket.on(MessageType.REQUEST_PEER, (peerId) => {
+    console.log(clientId + ' wants get data from ' + peerName);
+
+    clients[clientId].emit(
+      MessageType.REQUEST_PEER_ANSWER,
+      clients[peerId] ? clientData[peerId] : null,
+    );
+  });
+  
   socket.on(MessageType.RTC, (message) => {
     const remoteId = message.id;
 
@@ -65,6 +65,17 @@ io.on('connection', (socket) => {
       id: clientId,
     }));
   }); 
+  
+  socket.on(MessageType.DISCONNECT, () => {
+    console.log('disconnect ' + clientId);
+    clients[clientId].disconnect(true);
+
+    Object.keys(clients).forEach((key) => {
+      clients[key].emit(MessageType.LEAVE_PEER, clients[clientId].peerData.name);
+    });
+
+    delete clients[clientId];
+  });
 });
 
 server.listen(PORT);
